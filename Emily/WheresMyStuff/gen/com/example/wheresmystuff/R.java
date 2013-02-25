f05cb4acdd9be1211255ec65b698b353b1854676
Emily Getreu <egetreu@gatech.edu>
2013-02-25 16:24:05 -0500

    Updated black background for screens

--- Emily/WheresMyStuff/gen/com/example/wheresmystuff/R.java
+++ Emily/WheresMyStuff/gen/com/example/wheresmystuff/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int Black=0x7f060001;
         public static final int Blue=0x7f060000;
     }
     public static final class drawable {
