8a264b5ae51a9fc037c4a908e450fa8567c5a6d9
Emily Getreu <egetreu@gatech.edu>
2013-02-26 18:51:09 -0500

    main screen with image icons and search screen

--- Emily/WheresMyStuff/gen/com/example/wheresmystuff/R.java
+++ Emily/WheresMyStuff/gen/com/example/wheresmystuff/R.java
@@ -104,6 +104,7 @@ public final class R {
         public static final int add_item_screen=0x7f030001;
         public static final int another_file=0x7f030002;
         public static final int main_screen=0x7f030003;
+        public static final int search_screen=0x7f030004;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
