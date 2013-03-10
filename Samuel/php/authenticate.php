abc44d3a9c9a27a7ee1200b9858a56ea5ed2bdf2
Samuel Clarke <sclarke@gatech.edu>
2013-03-10 18:44:41 -0400

    AsyncTask modified Database

--- Samuel/php/authenticate.php
+++ Samuel/php/authenticate.php
@@ -1,11 +1,11 @@
 <?php
-require_once __DIR__ . '/db_connect.php';
+require_once('D:\Hosting\6939230\html\sherpas\db_connect.php');
 $db = new DB_CONNECT();
 
 $email = $_GET['email'];
 $password = $_GET['password'];
 
-$sql=mysql_query("SELECT ID, FirstName, LastName, Email FROM Person WHERE Email='$email' AND Password='$password';");
+$sql=mysql_query("SELECT ID, FirstName, LastName, Email, Phone FROM Person WHERE Email='$email' AND Password='$password';");
 while($row=mysql_fetch_assoc($sql))
 $output[]=$row;
 print(json_encode($output));
