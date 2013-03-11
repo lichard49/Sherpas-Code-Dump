81ae6d5ac8899aa3c8d5cb760038317bc65d4323
Samuel Clarke <sclarke@gatech.edu>
2013-03-10 20:45:08 -0400

    get list of all users

---
+++ Samuel/php/getAllUsers.php
@@ -0,0 +1,10 @@
+<?php
+require_once('D:\Hosting\6939230\html\sherpas\db_connect.php');
+$db = new DB_CONNECT();
+
+$sql=mysql_query("select FirstName, LastName, Email, Phone, ID from Person;");
+while($row=mysql_fetch_assoc($sql))
+$output[]=$row;
+print(json_encode($output));
+mysql_close();
+?>
