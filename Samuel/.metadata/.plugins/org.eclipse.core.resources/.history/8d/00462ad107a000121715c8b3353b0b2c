4bb4cd1673f21a4123296f0ea6d2290652cf8487
Samuel Clarke <sclarke@gatech.edu>
2013-04-10 00:22:29 -0400

    workspace stuff

---
+++ Samuel/.metadata/.plugins/org.eclipse.core.resources/.history/8d/00462ad107a000121715c8b3353b0b2c
@@ -0,0 +1,25 @@
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import edu.gatech.oad.wheres_my_stuff.model.Database;
+import junit.framework.TestCase;
+
+
+public class test extends TestCase {
+
+	public void testDatabase() {
+		Database d = Database.getInstance();
+	}
+
+	public void testGetInstance() {
+		Bitmap b = BitmapFactory.decodeFile("");
+	}
+
+	public void testCreateImage() {
+		assertTrue(true);
+	}
+
+	public void testGetImages() {
+		assertTrue(true);
+	}
+
+}
--- Samuel/php/filterItems.php
+++ Samuel/php/filterItems.php
@@ -6,40 +6,40 @@ $and = " AND ";
 if (isset($_GET['date']))
 {
 	$date = $_GET['date'];
-	$conditions .= "DatePosted >= '$date'" . $and;
+	$conditions .= "DatePosted >= '".$date."'" . $and;
 }
 if (isset($_GET['typeID']))
 {
 	$typeID = $_GET['typeID'];
-	$conditions .= "TypeID = $typeID" . $and;
+	$conditions .= "TypeID = ".$typeID . $and;
 }
 if (isset($_GET['categoryID']))
 {
 	$categoryID = $_GET['categoryID'];
-	$conditions .= "CategoryID = $categoryID" . $and;
+	$conditions .= "CategoryID = " . $categoryID . $and;
 }
 if (isset($_GET['name']))
 {
 	$name = $_GET['name'];
-	$conditions .= "Name LIKE '%$name%'" . $and;
+	$conditions .= "Name LIKE '%".$name."%'" . $and;
 }
 if (isset($_GET['description']))
 {
 	$description = $_GET['description'];
-	$conditions .= "Description LIKE '%$description%'" . $and;
+	$conditions .= "Description LIKE '%".$description."%'" . $and;
 }
 if (isset($_GET['city']))
 {
 	$city = $_GET['city'];
-	$conditions .= "City LIKE '%$city%'" . $and;
+	$conditions .= "City LIKE '%".$city."%'" . $and;
 }
 if (isset($_GET['state']))
 {
 	$state = $_GET['state'];
-	$conditions .= "State LIKE '%$state%'" . $and;
+	$conditions .= "State LIKE '%".$state."%'" . $and;
 }
 $conditions = substr($conditions, 0, -1*(strlen($and))); 
-$sql=mysql_query("select ID, Name, Description, TypeID, CategoryID, IsResolved, City, State, PosterID, DatePosted from Item WHERE "+$conditions+";");
+$sql=mysql_query("select ID, Name, Description, TypeID, CategoryID, IsResolved, City, State, PosterID, DatePosted from Item WHERE ".$conditions.";");
 while($row=mysql_fetch_assoc($sql))
 $output[]=$row;
 print(json_encode($output));
